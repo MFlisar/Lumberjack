e6d210fdca48f5300d2fe6a99b40a99915191b42
MFlisar <mflisar85@gmail.com>
2022-12-18 11:49:43 +0100

    * adjusted info log message color inside viewer if theme is dark * updated material version in viewer to 1.7.0

--- library/src/main/java/com/michaelflisar/lumberjack/core/CoreUtil.kt
+++ library/src/main/java/com/michaelflisar/lumberjack/core/CoreUtil.kt
@@ -2,6 +2,7 @@ package com.michaelflisar.lumberjack.core
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Color
 
 object CoreUtil {
 
@@ -17,5 +18,14 @@ object CoreUtil {
             e.printStackTrace()
             null
         }
+    }
+
+    internal fun Int.isColorDark(): Boolean {
+        val darkness: Double = this.getDarknessFactor()
+        return darkness >= 0.5
+    }
+
+    internal fun Int.getDarknessFactor(): Double {
+        return 1 - (0.299 * Color.red(this) + 0.587 * Color.green(this) + 0.114 * Color.blue(this)) / 255
     }
 }
--- library/src/main/java/com/michaelflisar/lumberjack/core/Level.kt
+++ library/src/main/java/com/michaelflisar/lumberjack/core/Level.kt
@@ -1,24 +1,26 @@
 package com.michaelflisar.lumberjack.core
 
 import android.graphics.Color
+import com.michaelflisar.lumberjack.core.CoreUtil.isColorDark
 
 enum class Level(
     val level: Int,
     private val color: Int? = null,
+    private val colorDark: Int? = color,
     private val marker: String? = null
 ) {
 
     TRACE(0),
     DEBUG(1),
-    INFO(2, Color.BLUE),
+    INFO(2, Color.BLUE, Color.parseColor("#ADD8E6") /* light blue */),
     WARN(3, Color.parseColor("#FFA500") /* orange */),
     ERROR(4, Color.RED),
-    WTF(5, Color.RED, "WTF-ERROR"),
+    WTF(5, Color.RED, marker = "WTF-ERROR"),
     UNKNOWN(-1, android.R.color.transparent)
     ;
 
     fun getTitleColor(textColor: Int): Int {
-        return color ?: textColor
+        return (if (textColor.isColorDark()) color else colorDark) ?: textColor
     }
 
     fun getTextColor(textColor: Int): Int {
