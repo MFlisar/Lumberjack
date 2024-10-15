0b705d510b193edae247a4d89f1ef20105869e63
Michael Flisar <Michael.Flisar@knapp.com>
2024-10-15 16:39:36 +0200

    getLatestLogFiles => getLatestLogFile

--- library/core/src/androidMain/kotlin/com/michaelflisar/lumberjack/core/FileLoggingExtensions.kt
+++ library/core/src/androidMain/kotlin/com/michaelflisar/lumberjack/core/FileLoggingExtensions.kt
@@ -3,4 +3,4 @@ package com.michaelflisar.lumberjack.core
 import com.michaelflisar.lumberjack.core.interfaces.IFileLoggingSetup
 
 fun IFileLoggingSetup.getAllExistingLogFiles() = getAllExistingLogFilePaths().map { it.toFile() }
-fun IFileLoggingSetup.getLatestLogFiles() = getLatestLogFilePath()?.toFile()
+fun IFileLoggingSetup.getLatestLogFile() = getLatestLogFilePath()?.toFile()
