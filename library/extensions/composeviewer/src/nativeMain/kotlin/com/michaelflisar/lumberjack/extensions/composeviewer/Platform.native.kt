85f3a77b880c42f3d55aa63b63a791faf30fed64
Michael Flisar <mflisar.development@gmail.com>
2025-05-30 18:40:16 +0200

    ...

--- library/extensions/composeviewer/src/nativeMain/kotlin/com/michaelflisar/lumberjack/extensions/composeviewer/Platform.native.kt
+++ library/extensions/composeviewer/src/nativeMain/kotlin/com/michaelflisar/lumberjack/extensions/composeviewer/Platform.native.kt
@@ -1,33 +1,52 @@
 package com.michaelflisar.lumberjack.extensions.composeviewer
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import okio.Path
+import com.michaelflisar.lumberjack.extensions.composeviewer.internal.IFeedbackProvider
 
 @Composable
-actual fun ShowLumberjackDialog(visible: MutableState<Boolean>, content: @Composable () -> Unit) {
-    Dialog(
-        properties = DialogProperties(usePlatformDefaultWidth = true),
-        onDismissRequest = { visible.value = false }
-    ) {
-        content()
+internal actual fun ShowLumberjackDialog(
+    visible: MutableState<Boolean>,
+    title: String,
+    content: @Composable () -> Unit
+) {
+    if (visible.value) {
+        Dialog(
+            onDismissRequest = { visible.value = false }
+        ) {
+            content()
+        }
     }
 }
 
-actual class FeedbackImpl actual constructor() {
-
-    actual fun supported() = false
-
-    @Composable
-    actual fun Init() {
+@Composable
+internal actual fun LazyScrollContainer(state: LazyListState, content: LazyListScope.() -> Unit) {
+    Box {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+            state = state
+        ) {
+            content()
+        }
+        VerticalScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(state)
+        )
     }
+}
 
-    actual fun sendFeedback(
-        receiver: String,
-        attachments: List<Path>
-    ) {
-    }
-
+internal actual fun getFeedbackImpl(): IFeedbackProvider {
+    return FeedbackImpl()
 }
